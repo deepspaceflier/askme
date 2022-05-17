63a029df49103e268ab9446afaedf492c94d6872
deepspaceflier <m4gp1e@yandex.ru>
2022-05-17 18:05:05 +0300

    initial commit

---
+++ db/migrate/20220113092922_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :questions do |t|
+      t.text :body
+      t.integer :user_id, index: true
+      t.boolean :hidden, default: false
+      
+
+      t.timestamps
+    end
+  end
+end
