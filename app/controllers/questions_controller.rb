2a218be82122787463700ff206737ee3e4672256
deepspaceflier <m4gp1e@yandex.ru>
2022-06-09 16:58:20 +0300

    added validations

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,15 +2,26 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: %i[update show destroy edit hide]
 
   def create 
-    question = Question.create(question_params)
+    question = Question.new(question_params)
 
-    redirect_to question_path(question), notice: 'Новый вопрос создан!'
+    if question.save
+      redirect_to question_path(question), notice: 'Новый вопрос создан!'
+    else
+      flash.now[:alert] = 'Вы неправильно заполнили поле вопроса'
+      
+      render :new
+    end
   end
 
   def update
-    @question.update(question_params)
+    if @question.update(question_params)
 
-    redirect_to question_path(@question), notice: 'Вопрос сохранен!'
+      redirect_to question_path(@question), notice: 'Вопрос сохранен!'
+    else
+      flash.now[:alert] = 'При попытки сохранить вопрос возникли ошибки'
+
+      render :edit
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
+
+  before_save :downcase_nickname
+ 
+  validates :email, presence: true, uniqueness: true, 
+            format: { with: /[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+/i }
+            
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, 
+            format: { with: /\A[a-z_]+\z/ }
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
