e1828590df6f024d489d8df161b39554fa77bedd
deepspaceflier <m4gp1e@yandex.ru>
2022-06-17 03:15:18 +0300

    created new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_175538) do
+ActiveRecord::Schema.define(version: 2022_06_17_000601) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_05_28_175538) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
 end
