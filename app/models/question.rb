4e502be0ae91b7549b3286520acdc7164e291022
deepspaceflier <m4gp1e@yandex.ru>
2022-06-23 02:06:32 +0300

    edited questions model and questions view

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,3 @@
 class Question < ApplicationRecord
-  validates :body, presence: true, length: { maximum: 280 }, format: { with: /^[\w ,.!?]+$/, multiline: true }
+  validates :body, presence: true, length: { maximum: 280 }
 end
